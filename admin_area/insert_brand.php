bc01850b7255023148da58a8950f6ba90dd543cd
hao759 <qhao74154@gmail.com>
2022-07-21 18:52:04 +0700

    insert category

--- admin_area/insert_brand.php
+++ admin_area/insert_brand.php
@@ -1,15 +1,22 @@
 <?php
 include('../include/connect.php');
-if (isset($_POST['insert_cat'])) {
-    $category_title = $_POST['cat_title'];
-    $inser_query = "insert into`category` (category_title) values('$category_title')";
-    $result = mysqli_query($con, $insert_query);
-    if ($result)
-    echo '<script>alert("Message")</script>';
-     // echo "<script>alert('Category has been inserted successfully')</script>";
+if (isset($_POST['insert_brand'])) {
+    $brand_title = $_POST['brand_title'];
+
+    // select data from database
+    $select_query = "Select * from`brand` where brand_title='$brand_title'";
+    $result_select = mysqli_query($con, $select_query);
+    $number = mysqli_num_rows($result_select);
+    if ($number > 0)
+        echo "<script>alert('This brand is present inside the database')</script>";
+    else {
+
+        $insert_query = "insert into `brand` (brand_title) values('$brand_title')";
+        $result = mysqli_query($con, $insert_query);
+        if ($result)
+            echo "<script> alert('$brand_title has been inserted successfully')</script>";
+    }
 }
-
-
 ?>
 
 
@@ -18,10 +25,10 @@ if (isset($_POST['insert_cat'])) {
 <form action="" method="post" class="mb-2">
     <div class="input-group w-90 mb-2">
         <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-        <input type="text" class="form-control" name="cat_title" placeholder="Insert Brand" aria-label="Username" aria-describedby="basic-addon1">
+        <input type="text" class="form-control" name="brand_title" placeholder="Insert Brand" aria-label="Username" aria-describedby="basic-addon1">
     </div>
     <div class="input-group w-10 mb-2">
-        <input type="submit" class="form-control  bg-info" name="insert_cat" value="Insert Categories" aria-label="Username" aria-describedby="basic-addon1" class="bg-info">
+        <input type="submit" class="form-control  bg-info" name="insert_brand" value="Insert Brand" aria-label="Username" aria-describedby="basic-addon1" class="bg-info">
         <!-- <button class="bg-info p-3" type="submit">Insert Brand</button> -->
     </div>
 </form>
